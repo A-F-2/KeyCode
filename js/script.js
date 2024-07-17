6a268ee038bf73c712f920667882352fdd7bc156
A-F-2 <dualac26@gmail.com>
2024-07-17 15:29:05 +0330

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+﻿let keyElem = document.querySelector("#key")
+
+let locationElem = document.querySelector("#location")
+
+let whichElem = document.querySelector("#which")
+
+let codeElem = document.querySelector("#code")
+
+let keyCodeElem = document.querySelector("#keyCode")
+
+document.body.addEventListener('keydown', function (event) {
+    if (event.code === "F12" || event.code === "CapsLock") {
+        event.preventDefault()
+        keyElem.innerHTML =  event.code
+        locationElem.innerHTML =  event.code
+        whichElem.innerHTML =  event.code
+        codeElem.innerHTML =  event.code
+        keyCodeElem.innerHTML =  event.code
+        starter.style.display = 'none'
+        heading.style.display = 'flex'
+        ascii.style.display = 'flex'
+        infos.style.display = 'flex'
+    } else {
+        keyCodeElem.innerHTML = event.which
+        keyElem.innerHTML = event.key
+        locationElem.innerHTML = event.location
+        whichElem.innerHTML = event.which
+        codeElem.innerHTML = event.code
+        starter.style.display = 'none'
+        heading.style.display = 'flex'
+        ascii.style.display = 'flex'
+        infos.style.display = 'flex'
+    }
+})
